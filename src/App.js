1f14b98ac3ba075946b0c81ff2a8809da2bbea7c
ADlv19 <adwaitg02@gmail.com>
2022-04-07 00:28:32 +0530

    NM

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 import Row from './Row';
 import requests from './Requests'
 import Quote from './Quote';
+import Banner from './Banner'
 
 function App() {
   return (
     <div className="App">
-      <h1>ADFlix</h1>
-      <h3>
-        <Quote/>
-      </h3>
-      <Row title="NotFlix ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumantaries} />
+      {
+        // Navigation
+      }
+      {<Banner />}
+      <div className='all_rows'>
+        <Row title="NotFlix ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumantaries} />
+      </div>
+      
 
       <h2 className="spirited_away">
         <iframe src="https://drive.google.com/file/d/1GkBsVNDn2kjWT-1XxznIBFaFtLeO3tPE/preview" width="640" height="480" allow="autoplay" title='Spirited Away'></iframe>
--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import "./Row.css"
 
 const POSTER_URL = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}){
+function Row({title, fetchUrl, isLargeRow}){
 
     const [movies, setMovies] = useState([]);
 
@@ -12,7 +12,6 @@ function Row({title, fetchUrl}){
         async function fetchData(){
             const requests = await axios.get(fetchUrl);
             setMovies(requests.data.results)
-            console.log(requests);
             return requests;
         }
         fetchData();
@@ -21,15 +20,14 @@ function Row({title, fetchUrl}){
     
     return(
         <div>
-            <h2> { title} </h2>
+            <h2> { title } </h2>
             <div className="row__posters">
                 {movies.map(movie => (
                     <img 
                         key = {movie.id}
-                        className="row__poster" 
-                        src={`${POSTER_URL}${movie.poster_path}`}
+                        className={`row__poster ${isLargeRow && "row__posterLarge"}`} 
+                        src={`${POSTER_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
                         alt={movie.name} />
-                        
                 ))}
             </div>
         </div>
