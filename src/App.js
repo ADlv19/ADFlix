defaf619aff9b70d3346672c4383a3e3068800cc
ADlv19 <adwaitg02@gmail.com>
2022-04-06 13:09:33 +0530

    NM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
-      Hello World This Is AD and My ADFlix app
+      <h1>Hello World This Is AD and My ADFlix app</h1>
+      <Row title="Trending"/>
+      <Row title="Originals"/>
     </div>
   );
 }
